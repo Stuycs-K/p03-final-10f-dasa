a23c0e14ed3464114bcf9c6cdd9461aafb1a2db5
Amrit Das <amritdas8015@gmail.com>
2026-01-09 12:27:07 -0500

    modifying client logic further

--- client.c
+++ client.c
@@ -48,7 +48,43 @@ void clientLogic(int server_socket){
   struct song_node *local_library = NULL;
   struct song_node *tail = NULL;
 
-  
+  for(int x = 0; x < count; x++){
+    struct song_node temps;
+    bytes = recv(server_socket, &temps, sizeof(struct song_node), 0);
+    if(bytes <= 0){
+      printf("Error reciving song %d.\n", x + 1);
+      break;
+    }
+
+    struct song_node *new_node = malloc(sizeof(struct song_node));
+    if(!new_node){
+      printf("Memory allocation failed.\n");
+      break;
+    }
+
+    strcpy(new_node-> name, temps.name);
+    strcpy(new_node-> artist, temps.artist);
+    new_node -> next = NULL;
+
+    if(local_library == NULL){
+      local_library = new_node;
+      tail = new_node;
+    } else{
+      tail->next = new_node;
+      tail = new_node;
+    }
+  }
+  close(server_socket);
+  printf("Local Sub-Library\n");
+  struct song_node *curr = local_library;
+  while(curr != NULL){
+    printf("%s - %s\n", curr -> artist, curr-> name);
+    curr = curr-> next;
+  }
+
+  printf("\n");
+
+
 }
 
 int main(int argc, char *argv[] ) {
